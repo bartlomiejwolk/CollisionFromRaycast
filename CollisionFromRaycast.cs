93bec5edade5d1dff72840901f94c993cb2960ba
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-11 14:58:42 +0200

    Initial commit

---
+++ CollisionFromRaycast.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace OneDayGame {
+
+	[ExecuteInEditMode]
+	public class CollisionFromRaycast : GameComponent, ICollision {
+
+		/// If collision happened.
+		[SerializeField]
+		private bool _collision = false;
+
+		public bool Collision {
+			get { return _collision; }
+			private set {}
+		}
+
+		/// Raycast length.
+		[SerializeField]
+		private float _raycastLength = 1;
+		public float RaycastLength {
+			get { return _raycastLength; }
+			set { _raycastLength = value; }
+		}
+
+		/// Contain info about collied object.
+		protected RaycastHit _hit;
+		public RaycastHit Hit {
+			get { return _hit; }
+			set { _hit = value; }
+		}
+
+		/// Layer on which collisions should be detected
+		[SerializeField]
+		private LayerMask _includeLayerMask;
+
+		public LayerMask IncludeLayerMask {
+			get { return _includeLayerMask; } 
+			set { _includeLayerMask = value; }
+		}
+
+		/// Object that has been hit
+		protected GameObject _hitObject;
+
+		public GameObject HitObject {
+			get { return _hitObject; }
+			set { _hitObject = value; }
+		}
+
+		/// Draw raycast.
+		[SerializeField]
+		private bool _drawRay;
+
+		// TODO Add doc
+		public bool DrawRay {
+			get { return _drawRay; }
+			set { _drawRay = value; }
+		}
+
+		/// Pause game on collision.
+		[SerializeField]
+		private bool _pauseGame;
+
+		// TODO
+		public bool PauseGame {
+			get { return _pauseGame; }
+			set { _pauseGame = value; }
+		}
+
+		/// Disable component after collision.
+		[SerializeField]
+		private bool _disableAfterCollision;
+
+		public override void Start () {
+			base.Start();
+		}
+		
+		public override void Update () {
+			base.Update();
+		}
+
+		public override void FixedUpdate() {
+			// Don't execute in edit mode
+			if (!Application.isPlaying) {
+				return;
+			}
+			
+			// TODO Rename to CheckForCollision()
+			if (CollisionDetected()) {
+				_collision = true;
+				/// Disable component.
+				if (_disableAfterCollision) {
+					this.enabled = false;
+				}
+			}
+			else {
+				_collision = false;
+			}
+			
+			// Break on collision if the option is set.
+			if (_collision && _pauseGame) {
+				Debug.Log("CollisionFromRaycast: stop on collision");
+				Debug.Break();
+			}
+
+			if (_drawRay) {
+				Debug.DrawRay(transform.position,
+						transform.forward * _raycastLength, Color.green);
+			}
+		}
+
+		/// Check for collision with another object.
+		private bool CollisionDetected() {
+			bool collision;
+
+			collision = Physics.Raycast(
+					transform.position,
+					transform.forward,
+					out _hit,
+					_raycastLength,
+					_includeLayerMask);
+			if(collision) {
+				_hitObject = _hit.collider.gameObject;
+				return true;
+			}
+			return false;
+		}
+	}
+}
---
+++ Editor/CollisionFromRaycastEditor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using OneDayGame;
+
+[CustomEditor(typeof(CollisionFromRaycast))]
+public class CollisionFromRaycastEditor : GameComponentEditor {
+
+	private SerializedProperty _raycastLength;
+	private SerializedProperty _drawRay; 
+	private SerializedProperty _pauseGame;
+	private SerializedProperty _disableAfterCollision;
+
+	public override void OnEnable() {
+		base.OnEnable();
+		_raycastLength = serializedObject.FindProperty("_raycastLength");
+		_drawRay = serializedObject.FindProperty("_drawRay");
+		_pauseGame = serializedObject.FindProperty("_pauseGame");
+		_disableAfterCollision = serializedObject.FindProperty("_disableAfterCollision");
+	}
+
+	public override void OnInspectorGUI() {
+		base.OnInspectorGUI();
+		//CollisionFromRaycast script = (CollisionFromRaycast)target;
+		serializedObject.Update();
+		
+		EditorGUILayout.PropertyField(_raycastLength);
+		EditorGUILayout.PropertyField(_drawRay);
+		EditorGUILayout.PropertyField(_pauseGame);
+		EditorGUILayout.PropertyField(_disableAfterCollision);
+
+		serializedObject.ApplyModifiedProperties();
+	}
+}
