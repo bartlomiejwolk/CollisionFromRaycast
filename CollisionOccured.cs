247b82b9bbebb45c0fe8ba9caccffe18d301d0db
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-19 16:32:00 +0200

    Add licence info

--- CollisionOccured.cs
+++ CollisionOccured.cs
@@ -1,3 +1,8 @@
+// Copyright (c) 2015 Bart³omiej Wo³k (bartlomiejwolk@gmail.com)
+// 
+// This file is part of the CollisionFromRaycast extension for Unity.
+// Licensed under the MIT license. See LICENSE file in the project root folder.
+
 using UnityEngine;
 using UnityEngine.Events;
 
--- Editor/CollisionFromRaycastEditor.cs
+++ Editor/CollisionFromRaycastEditor.cs
@@ -1,4 +1,9 @@
-﻿using UnityEngine;
+﻿// Copyright (c) 2015 Bartłomiej Wołk (bartlomiejwolk@gmail.com)
+// 
+// This file is part of the CollisionFromRaycast extension for Unity.
+// Licensed under the MIT license. See LICENSE file in the project root folder.
+
+using UnityEngine;
 using System.Collections;
 using UnityEditor;
 using OneDayGame;
